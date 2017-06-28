7e4458a8fefb3339ba01817051791830dce7dd40
Horacio <horacio.gonzalez@gmail.com>
2017-06-28 11:59:54 +0200

    Added Slim.js

--- step-05/my-slim-counter.js
+++ step-05/my-slim-counter.js
@@ -1,18 +1,66 @@
-Slim.tag('my-slim-counter', class extends Slim {
-    get template() {
-        return '<div style="display: flex; flex-flow: row wrap;font-size: 5rem;justify-content: space-around; align-items: center; background-color: rgb(170, 255, 170); padding: 1rem; border-radius: 0.5rem;"><div style="width: 7rem; height: 7rem; border-radius: 1rem; margin: 0.5rem; display: flex; flex-flow: row nowrap; justify-content: center; align-items: center; background-color: rgb(221, 221, 221); cursor: pointer; border-width: 2px; border-style: outset; border-color: buttonface;"><img src="./img/web-components.png"></div><div style="margin: 0.5rem;"> [[counter]] </div></div>';
+Slim.tag('my-slim-counter', `
+    <style>
+      my-slim-counter .container {
+        display: block;
+        font-size: 5rem;
+        border-radius: 0.5rem;
+        padding: 1rem;
+        background-color: #ffbb00;
+      }
+      my-slim-counter .button {
+        width: 7rem;
+        height: 7rem;
+        border-radius: 1rem;
+        margin: 0.5rem;
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+        align-items: center;
+        background-color: #ddd;
+        cursor: pointer;
+        border-width: 2px;
+        border-style: outset;
+        border-color: buttonface;
+        border-image: initial;
+      }
+      my-slim-counter .container {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-around; 
+        align-items: center;
+      }
+      my-slim-counter .value {
+        margin: 0.5rem;
+      }
+    </style> 
+    <div class="container">
+        <div class="button"  slim-id="button">
+            <img src="./img/slim.png">
+        </div>
+        <div class="value" bind> [[counter]] </div>
+    </div>`, 
+
+class extends Slim {
+
+
+    onBeforeCreated() {
+        // console.log('Going to be created')
     }
-    onBeforeRender() {
-        this.counter = this.getAttribute('counter'); 
-        console.log(this.counter)
-    }
-    static get observedAttributes() { return [ 'counter' ] }
-    attributeChangedCallback(attr, oldVal, newVal) {
-        console.log(attr);
-        if (oldVal !== newVal) {
-            this[attr] = newVal;
+    
+    onBeforeUpdate() {
+        // console.log( this.counter, this.getAttribute('counter'), Number.parseInt(this.getAttribute('counter')))
+        if (this.counter == undefined) {
+            this.counter = Number.parseInt(this.getAttribute('counter'))||0;
         }
-        console.log(this.counter)
+    }
+    onCreated() {
+        this.button.onclick = () => {
+            this.counter++;
+            this.dispatchEvent(new CustomEvent('counter-changed', {detail: {counter: this.counter}}));
+        }
     }
 });
+
+// console.log(document.createElement('my-slim-counter').constructor)
+
 
